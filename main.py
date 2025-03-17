0cc9b5f8f4588994f31f528656bd4791c964e962
Bayan Mardon <mardon.junior@gmail.com>
2025-03-17 15:04:30 -0400

    add code to trim whitespace within a line

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ class Parser():
       for (i, line) in enumerate(self.lines):
         self.lines[i] = line.strip()
     return self.lines
+
+  def _trim_whitespace(self, text):
+    return ' '.join(text.split())
   
   def write_to_file(self):
     with open(f"{file_name}.txt", "w") as file:
@@ -18,9 +21,10 @@ class Parser():
       for line in self.lines:
         if line == '':
           continue
+        line = self._trim_whitespace(line)
         file.write(line + "\n")
 
-
+  
 if __name__ == "__main__":
   file_name = input("Please enter the name of the file you want to clean (path only, no extension): ")
   parser = Parser(file_name)
